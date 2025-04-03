a91774154be5af8159f4ab438fc439ce82325bc8
Alonso Lagos <a.lagos10@ufromail.cl>
2025-04-03 10:06:55 -0300

    -- creado menu y readme --

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,54 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[][] matriz = crearMatriz();
+        imprimirMatriz(matriz);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    }
+
+
+
+
+    public static void llenarMatriz(int [][]m){
+        for (int i = 0; i < m.length; i++){
+            for (int j = 0; j < m.length; j++){
+                System.out.println("Inserte el valor para la posicion ["+i+"]["+j+"]");
+                m[i][j] = obtenerNumero();
+            }
+        }
+
+    }
+
+
+    public static int[][] crearMatriz(){
+        int[][] matrix = new int[2][2];
+        llenarMatriz(matrix);
+
+        return matrix;
+    }
+
+    public static int obtenerNumero(){
+        Scanner scanner = new Scanner(System.in);
+        while (!scanner.hasNextInt()){
+            System.out.println("Número inválido, por favor ingresar un numero entero.");
+            scanner.next();
+        }
+        return scanner.nextInt();
+    }
+
+    public static void imprimirMatriz(int m[][]){
+        for (int i=0; i<m.length;i++){
+            for (int j=0;j<m.length;j++){
+                System.out.println(m[i][j]);
+            }
         }
     }
+
+
+
 }
